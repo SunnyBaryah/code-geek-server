81b785136640ce86c4a15db03f3453af231a0d78
SunnyBaryah <130865728+SunnyBaryah@users.noreply.github.com>
2024-11-23 18:46:33 +0530

    update index.js file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Mongo DB connection failed!! ", err);
-  });
+let isConnected = false;
+
+export default async function handler(req, res) {
+  if (!isConnected) {
+    try {
+      await connectDB();
+      isConnected = true;
+    } catch (error) {
+      console.error("Database connection error:", error);
+      return res.status(500).send("Internal server error");
+    }
+  }
+  app(req, res);
+}
